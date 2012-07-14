88b9570dd21ee34fd2a08dba4217a484893b852f
Josh Palmer <jpalmerwatkins@me.com>
2012-07-14 19:56:00 +0100

    Update SystemUiController.cpp
    Catch mouse events on the gesture border, stops them propagating to
    apps.

--- Src/base/SystemUiController.cpp
+++ Src/base/SystemUiController.cpp
@@ -63,6 +63,7 @@ static const char* kSystemUiAppId = "com.palm.systemui";
 static const unsigned int s_statusBarLauncherColor = 0x4f545AFF;
 static const unsigned int s_statusBarJustTypeColor = 0x4f545AFF;
 
+static const int kGestureBorderSize = 15;
 static const int kFlickMinimumYLengthWithKeyboardUp = 60;
 
 SystemUiController* SystemUiController::instance()
@@ -243,6 +244,53 @@ bool SystemUiController::handleEvent(QEvent *event)
 
 bool SystemUiController::handleMouseEvent(QMouseEvent *event)
 {
+	if(event->type() == QEvent::MouseButtonPress)
+	{
+		//Adhere to 'Enable Advanced Gestures'
+		if (!Preferences::instance()->sysUiEnableNextPrevGestures()) return false;
+		
+		int INVALID_COORD = 0xFFFFFFFF;
+		int xDown = INVALID_COORD;
+		int yDown = INVALID_COORD;
+
+		xDown = event->pos().x();
+		yDown = event->pos().y();
+
+		//Transform touch coordinates to match the screen orientation
+		switch (WindowServer::instance()->getUiOrientation())
+		{
+			case OrientationEvent::Orientation_Up: //Speakers Down
+				//Do nothing
+				break;
+			case OrientationEvent::Orientation_Down: //Speakers Up
+				xDown = (m_uiWidth-1) - xDown;
+				yDown = (m_uiHeight-1) - yDown;
+				break;
+			case OrientationEvent::Orientation_Left: //Speakers Right
+			{
+				int temp = (m_uiHeight-1) - xDown;
+				xDown = yDown;
+				yDown = temp;
+				break;
+			}
+			case OrientationEvent::Orientation_Right: //Speakers Left
+			{
+				int temp = xDown;
+				xDown = (m_uiWidth-1) - yDown;
+				yDown = temp;
+				break;
+			}
+			default:
+				g_warning("Unknown UI orientation");
+				return false;
+		}
+
+		//Eat mousedown events on the gesture border
+		if (xDown <= kGestureBorderSize && yDown > m_statusBarPtr->boundingRect().height()) return true;
+		if (xDown >= (m_uiWidth-1) - kGestureBorderSize && yDown > m_statusBarPtr->boundingRect().height()) return true;
+		if (yDown >= (m_uiHeight-1) - kGestureBorderSize) return true;
+	}
+	
 	return false;
 }
 
