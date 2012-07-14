eea9079e8edfd7080f4b7fbbbb307a459f58f2bb
Josh Palmer <jpalmerwatkins@me.com>
2012-07-14 20:33:46 +0100

    Update StatusBarItemGroup.cpp
    Add a trigger to mouseMoveEvent to open an item group, make it check
    against sysUiStatusBarSlide.

--- Src/lunaui/status-bar/StatusBarItemGroup.cpp
+++ Src/lunaui/status-bar/StatusBarItemGroup.cpp
@@ -20,6 +20,7 @@
 
 #include "StatusBarItemGroup.h"
 #include "Settings.h"
+#include "Preferences.h"
 #include "StatusBarItem.h"
 #include <QPainter>
 #include "AnimationSettings.h"
@@ -322,7 +323,13 @@ void StatusBarItemGroup::mousePressEvent(QGraphicsSceneMouseEvent* event)
 
 void StatusBarItemGroup::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
 {
-
+	if(Preferences::instance()->sysUiStatusBarSlide())
+	{
+		if(m_actionable && m_mouseDown && (int)event->pos().y() >= (int)event->buttonDownPos(Qt::LeftButton).y() + 15) {
+			actionTriggered();
+			m_mouseDown = false;
+		}
+	}
 }
 
 void StatusBarItemGroup::mouseDoubleClickEvent(QGraphicsSceneMouseEvent* event)
