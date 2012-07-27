603c1811c82ffe334347c1ce6f6859d54f724769
Josh Palmer <jpalmerwatkins@me.com>
2012-07-28 00:59:08 +0100

    Additions to CardSwitchGesture.h
    Converted getters to const, added m_edge & related methods.

--- Src/base/gesture/CardSwitchGesture.h
+++ Src/base/gesture/CardSwitchGesture.h
@@ -38,16 +38,19 @@ class CardSwitchGesture : public QGesture
 public:
 
 	CardSwitchGesture(QObject* parent = 0) : QGesture(parent, (Qt::GestureType) GestureCardSwitch) {}
-    QPointF pos() { return m_pos; }
+    QPointF pos() const { return m_pos; }
     void setPos(QPointF pos) { m_pos = pos; }
-    QPointF lastPos() { return m_lastPos; }
+    QPointF lastPos() const { return m_lastPos; }
     void setLastPos(QPointF lastPos) { m_lastPos = lastPos; }
-    int flick() { return m_flick; }
+    int flick() const { return m_flick; }
     void setFlick(int flick) { m_flick = flick; }
+    bool edge() const { return m_edge; }
+    void setEdge(bool edge) { m_edge = edge; }
 
 private:
     QPointF m_pos;
     QPointF m_lastPos;
+    bool m_edge;
     int m_flick;
 
 private:
