edb8597e65cd7fb054ad4a6bf51657460ed0cf7b
Josh Palmer <jpalmerwatkins@me.com>
2012-08-08 23:59:40 +0100

    Revert "Merge branch 'dynamic-launcher-layout' into alpha-testing"
    This reverts commit 395a074bda0d135831b31501f13674732b525fe0, reversing
    changes made to 69cf2c3694f184ad018020b035259a9e891899ea.

--- Src/lunaui/launcher/elements/page/icon_layouts/iconlayoutsettings.cpp
+++ Src/lunaui/launcher/elements/page/icon_layouts/iconlayoutsettings.cpp
@@ -167,7 +167,7 @@ IconLayoutSettings::IconLayoutSettings()
 , reorderablelayout_emptyPageTextFontColor(Qt::white)
 
 , reorderablelayout_emptyPageTextOffsetFromCenter(0,-100)
-, reorderablelayout_emptyPageIconOffsetFromCenter(0,50)
+, reorderablelayout_emptyPageIconOffsetFromCenter(0,0)
 {
 	s_instance = this;
 
