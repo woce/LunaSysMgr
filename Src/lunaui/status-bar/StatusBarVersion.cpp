462f5e1ae8bd2dee0e99bf19e98a9a3adc0ebd5a
Josh Palmer <jpalmerwatkins@me.com>
2012-08-29 01:22:02 +0100

    Bump version string to 4.5.0, beta here we come!

--- Src/lunaui/status-bar/StatusBarVersion.cpp
+++ Src/lunaui/status-bar/StatusBarVersion.cpp
@@ -30,7 +30,7 @@
 
 #define TEXT_BASELINE_OFFSET            (-1)
 
-static const QString currentVersion = "WebOS Ports LunaCE 4.4.2";
+static const QString currentVersion = "WebOS Ports LunaCE 4.5.0";
 
 StatusBarVersion::StatusBarVersion(unsigned int padding)
 	: m_font(0)
